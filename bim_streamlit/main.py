f33aff00474920f6d2856f2849e6d17c60f6689e
hasann <hasann71@proton.me>
2024-10-20 10:12:56 +0300

    removing history from display and add the translation

--- bim_streamlit/main.py
+++ bim_streamlit/main.py
@@ -53,20 +53,15 @@ if "messages" not in st.session_state:
     ]
 
 # Display chat messages from history on app rerun
-with placeholder.container():
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"], unsafe_allow_html=True)
+#with placeholder.container():
+#    for message in st.session_state.messages:
+#        with st.chat_message(message["role"]):
+#            st.markdown(message["content"], unsafe_allow_html=True)
 
 # User input - switch between sidebar sample quick select or actual user input. Clunky but works.
 #print("before free_questions_exhausted",free_questions_exhausted(), user_supplied_openai_key_unavailable() )
 
-if free_questions_exhausted() and user_supplied_openai_key_unavailable():
-    st.warning(
-        "Thank you for trying out the Auto Civil Engineer. Please input your OpenAI Key in the sidebar to continue asking questions."
-    )
-    st.stop()
-#print("after free_questions_exhausted")
+
 if "sample" in st.session_state and st.session_state["sample"] is not None:
     user_input = st.session_state["sample"]
 else:
@@ -77,7 +72,7 @@ else:
 if user_input:
     with user_placeholder.container():
         track("rag_demo", "question_submitted", {"question": user_input})
-        st.session_state.messages.append({"role": "user", "content": user_input})
+#        st.session_state.messages.append({"role": "user", "content": user_input})
         with st.chat_message("user"):
             st.markdown(user_input)
 
@@ -92,59 +87,65 @@ if user_input:
                 agent_response=None
                 intermediate_steps=None
                 if(st.session_state["USER_SELECTION"]=="DOCUMENTATION"):
-                    agent_response, cb=rag_tool.run(tool_input=user_input)    
+                    agent_response, cb=rag_tool.run(tool_input=user_input)  
+                    content = str(agent_response)
+                    track(
+                        "rag_demo", "ai_response", {"type": "rag_agent", "answer": content}
+                    )
+                    req_message = {"role": "user", "content": user_input}
+                    resp_message = {"role": "ai", "content": content}
+
+                    with placeholder.container():
+                        with st.chat_message(req_message["role"]):
+                            st.markdown(req_message["content"], unsafe_allow_html=True)                        
+                        with st.chat_message(resp_message["role"]):
+                            st.markdown(resp_message["content"], unsafe_allow_html=True)
                 elif(st.session_state["USER_SELECTION"]=="RELATED_POSTS"):
                     agent_response, cb=object_tool.run(tool_input=user_input)  
                     data = []
                     for doc in agent_response:
                         # Flatten the metadata dictionary and add it to the list
-                        row = {"content": doc.page_content}
+                        row = { "translation": doc.page_content  }
                         row.update(doc.metadata)
                         data.append(row)
-
                     # Convert the list of dictionaries to a Pandas DataFrame
                     df = pd.DataFrame(data)
                     df = df.loc[:, (df != 0).any(axis=0)]
+                    df["collecting_time"]=pd.to_datetime(df['collecting_time'], unit='s')
+                    df["publish_time"]=pd.to_datetime(df['publish_time'], unit='s')
                     # Display the DataFrame as a table in Streamlit
                     st.dataframe(df)
-                if isinstance(agent_response, dict) is False:
-                    logging.warning(
-                        f"Agent response was not the expected dict type: {agent_response}"
-                    )
-                    agent_response = str(agent_response)
-                content = str(agent_response)
-                track(
-                    "rag_demo", "ai_response", {"type": "rag_agent", "answer": content}
-                )
-                new_message = {"role": "ai", "content": content}
-                st.session_state.messages.append(new_message)
-                generated_cypher=None
-                if intermediate_steps:
-                    generated_cypher=intermediate_steps[0]["query"].replace("cypher", "")
-                    st.write(f"generated_cypher: {generated_cypher}" )
-                st.write(f"query info: {cb}" )
-                decrement_free_questions()
+#                if isinstance(agent_response, dict) is False:
+#                    agent_response = str(agent_response)
+#                content = str(agent_response)
+#                track(
+#                    "rag_demo", "ai_response", {"type": "rag_agent", "answer": content}
+#                )
+#                new_message = {"role": "ai", "content": content}
+#                st.session_state.messages.append(new_message)
 
-            message_placeholder.markdown(content)
+#                st.write(f"query info: {cb}" )
+
+#            message_placeholder.markdown(content)
 
 
     # Reinsert user chat input if sample quick select was previously used.
-    if "sample" in st.session_state and st.session_state["sample"] is not None:
-        st.session_state["sample"] = None
-        user_input = st.chat_input(
-            placeholder="Ask question on the SEC Filings", key="user_input"
-        )
+#    if "sample" in st.session_state and st.session_state["sample"] is not None:
+#        st.session_state["sample"] = None
+#        user_input = st.chat_input(
+#            placeholder="Ask question on the SEC Filings", key="user_input"
+#        )
 
-    emoji_feedback = st.empty()
+#    emoji_feedback = st.empty()
 
 # Emoji feedback
-with emoji_feedback.container():
-    feedback = streamlit_feedback(feedback_type="thumbs")
-    if feedback:
-        score = feedback["score"]
-        last_bot_message = st.session_state["messages"][-1]["content"]
-        track(
-            "rag_demo",
-            "feedback_submitted",
-            {"score": score, "bot_message": last_bot_message},
-        )
+#with emoji_feedback.container():
+#    feedback = streamlit_feedback(feedback_type="thumbs")
+#    if feedback:
+#        score = feedback["score"]
+#        last_bot_message = st.session_state["messages"][-1]["content"]
+#        track(
+#            "rag_demo",
+#            "feedback_submitted",
+#            {"score": score, "bot_message": last_bot_message},
+#        )
